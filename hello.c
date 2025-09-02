ac22a0811b33ec4dcbe34264403019d46a77759b
Kevygutierrez <kgutierrez10@huskers.unl.edu>
2025-09-02 22:56:46 +0000

    Tue, Sep 2, 2025, 5:56 PM -05:00

--- hello.c
+++ hello.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
     int main(){
-        printf("Hello my name is Kevyn?\n");
+        printf("Hello my name is Kevyn!\n");
         printf("My major is Electrical.\n");
     }
